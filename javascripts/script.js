16b5224ade69d173f34190554c4e6898a1c0d977
Jessa Wright <jessa.wright@gmail.com>
2016-04-04 11:44:41 -0500

    Create script.js

---
+++ javascripts/script.js
@@ -0,0 +1,336 @@
+$(document).ready(function() {
+  // My JSON of content
+  var quotes = [
+    // Mildred Davis talking gif
+    [{
+      //Davis 1
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845559/davis_talking_1_mvf3yp.gif",
+      "source": "High and Dizzy (1920)",
+      "link": "http://littlehorrorshop.tumblr.com/post/83154252546/mildred-davis-in-high-and-dizzy-1920"
+    }, {
+      //Davis 2
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845559/davis_talking_1_mvf3yp.gif",
+      "source": "From Hand to Mouth (1919)",
+      "link": "http://littlehorrorshop.tumblr.com/post/129274834463/mildred-davis-in-from-hand-to-mouth-1919"
+    }, {
+      //Davis 3
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845559/davis_talking_3_nh76tl.gif",
+      "source": "Grandma’s Boy (1922)",
+      "link": "http://mildreddavis.tumblr.com/post/4709616283/mildred-davis-in-grandmas-boy-1922"
+    }, {
+      //Davis 4
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845562/davis_talking_4_rw0l4x.gif",
+      "source": "Dr. Jack (1922)",
+      "link": "http://littlehorrorshop.tumblr.com/post/130116349588/mildred-davis-is-cheeky-in-dr-jack-1922"
+    }, {
+      //Davis 5
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845561/davis_talking_5_ucucdy.gif",
+      "source": "Haunted Spooks (1920)",
+      "link": "http://fyeah-haroldlloyd.tumblr.com/post/124646432657/mildred-davis-haunted-spooks-1920"
+    }],
+
+    //Love question text
+    [{
+      "quote": "Isn’t it wonderful to be in love?",
+      "source": "The Freshman (1925)"
+    }, {
+      "quote": "Do you love me?",
+      "source": "Grandma’s Boy (1922)"
+    }, {
+      "quote": "Will you marry me?",
+      "source": "High and Dizzy (1920)"
+    }, {
+      "quote": "Do you believe in love at first sight?",
+      "source": "High and Dizzy (1920)"
+    }, {
+      "quote": "Do you think you could learn to love me?",
+      "source": "Battling Butler (1926)"
+    }],
+    // Harold Lloyd talking gif
+    [{
+      // Lloyd 1
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458847813/lloyd_talking_1_nyhpeh.gif",
+      "source": "The Freshman (1925)",
+      "link": "http://haroldlloyds.tumblr.com/post/81468084592"
+    }, {
+      // Lloyd 2
+      "media": "http://res.cloudinary.com/jessawright/image/upload/c_crop,g_north,h_375,w_500/v1458845564/lloyd_talking_2_ox9n3r.gif",
+      "source": "Safety Last (1923)",
+      "link": "http://littlehorrorshop.tumblr.com/post/103772774338/harold-lloyd-in-safety-last-1923"
+    }, {
+      // Lloyd 3
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845563/lloyd_talking_3_ambxpx.gif",
+      "source": "High and Dizzy (1920)",
+      "link": "http://fyeah-haroldlloyd.tumblr.com/post/139070303187/fyeah-haroldlloyd-mildred-davis-and-harold"
+    }, {
+      // Lloyd 4
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845565/lloyd_talking_4_dryior.gif",
+      "source": "Now or Never (1921)",
+      "link": "http://fyeah-haroldlloyd.tumblr.com/post/141054315837/fyeah-haroldlloyd-anna-mae-bilson-doesnt-care"
+    }, {
+      // Lloyd 5
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845562/lloyd_talking_5_unevou.gif",
+      "source": "The Freshman (1925)",
+      "link": "http://littlehorrorshop.tumblr.com/post/37184489648/harold-lloyds-tongue-disheveled-hair-and-freaky"
+    }, {
+      // Lloyd 6
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845563/lloyd_talking_6_vmn4wn.gif",
+      "source": "The Freshman (1925)",
+      "link": "http://littlehorrorshop.tumblr.com/post/37111764342/jobyna-ralston-and-harold-lloyd-in-the-freshman"
+    }, {
+      // Lloyd 7
+      "media": "http://res.cloudinary.com/jessawright/image/upload/c_crop,g_north,h_375,w_500/v1458845564/lloyd_talking_7_icft7w.gif",
+      "source": "Get Out and Get Under (1920)",
+      "link": "http://haroldlloyds.tumblr.com/post/37107232944"
+    }, {
+      // Lloyd 8
+      "media": "http://res.cloudinary.com/jessawright/image/upload/c_crop,g_north,h_375,w_500,y_30/v1458845568/lloyd_talking_8_q8trae.gif",
+      "source": "Safety Last (1923)",
+      "link": "http://littlehorrorshop.tumblr.com/post/90305390278/harold-lloyd-in-safety-last-1923"
+    }],
+    //Love answer text
+    [{
+      "quote": "Certainly.",
+      "source": "The Last Warning (1929)"
+    }, {
+      "quote": "Constantly!",
+      "source": "Seven Chances (1925)"
+    }, {
+      "quote": "YES!",
+      "source": "Girl Shy (1924)"
+    }, {
+      "quote": "I LOVE YOU, I LOVE YOU, I LOVE YOU",
+      "source": "Grandma’s Boy (1922)"
+    }, {
+      "quote": "I never knew until now how much I love you.",
+      "source": "The Extra Girl (1923)"
+    }],
+    // Davis or both talking
+    [{
+      // Both Davis talking
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845557/both_davis_talking_1_worpxu.gif",
+      "source": "High and Dizzy (1920)",
+      "link": "http://littlehorrorshop.tumblr.com/post/83278563454/mildred-davis-and-harold-lloyd-in-high-and-dizzy"
+    }, {
+      // Together 2
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845566/together_2_pylrai.gif",
+      "source": "Safety Last (1923)",
+      "link": "http://haroldlloyds.tumblr.com/post/75020439841"
+    }, {
+      // Together 3
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845565/together_3_qtclyu.gif",
+      "source": "Grandma’s Boy (1922)",
+      "link": "http://mizenscen.tumblr.com/post/3942782710/mildred-davis-harold-lloyd-grandmas-boy"
+    }, {
+      // Together 4
+      "media": "http://res.cloudinary.com/jessawright/image/upload/c_crop,g_north,h_375,w_500/v1458845566/together_4_xckob8.gif",
+      "source": "From Hand to Mouth (1919)",
+      "link": "http://nitratediva.tumblr.com/post/74008436290/mildred-davis-and-harold-lloyd-in-from-hand-to"
+    }],
+    //Love Statement
+    [{
+      "quote": "I’m certainly glad to have met you!",
+      "source": "City Girl (1930)"
+    }, {
+      "quote": "Bless your dear heart, I’m ready at last—I’ll marry you whenever you say the word.",
+      "source": "Never Weaken"
+    }, {
+      "quote": "Remember, whatever happens, I will always love you.",
+      "source": "His Majesty, The Scarecrow of Oz (1914)"
+    }, {
+      "quote": "At last I have a chance to kiss a real man.",
+      "source": "Take a Chance (1918)"
+    }, {
+      "quote": "You’re so beautiful I feel like I’m wasting my time when I’m not looking at you.",
+      "source": "Spring Fever (1927)"
+    }, {
+      "quote": "A dame like you could make me wear balloon pants.",
+      "source": "My Lady of Whims (1925)"
+    }, {
+      "quote": "But dear, ours is a love that is above such stupid conventions.",
+      "source": "Stolen Moments (1920)"
+    }, {
+      "quote": "— Then— If I’m your little tootsie wootsie— Where’s the ring?",
+      "source": "Teddy at the Throttle (1917)"
+    }],
+    // Lloyd or both talking
+    [{
+      // Both Lloyd talking 1
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845558/both_lloyd_talking_1_hfz8fs.gif",
+      "source": "High and Dizzy (1920)",
+      "link": "http://littlehorrorshop.tumblr.com/post/83278563454/mildred-davis-and-harold-lloyd-in-high-and-dizzy"
+    }, {
+      // Both Lloyd talking 2
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845559/both_lloyd_talking_2_jqqxp2.gif",
+      "source": "Safety Last (1923)",
+      "link": "http://haroldlloyds.tumblr.com/post/75020439841"
+    }, {
+      // together 1
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845566/together_1_moynrp.gif",
+      "source": "High and Dizzy (1920)",
+      "link": "http://littlehorrorshop.tumblr.com/post/83278563454/mildred-davis-and-harold-lloyd-in-high-and-dizzy"
+    }, {
+      // Both kiss 5
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845557/both_kiss_5_jbb85b.gif",
+      "source": "Never Weaken (1921)",
+      "link": "http://rebloggy.com/post/gif-harold-lloyd-mildred-davis-this-is-the-worst-gif-ive-ever-made-neverweaken/51293720985"
+    }, {
+      // Both kiss 6
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845558/both_kiss_6_ffg1rv.gif",
+      "source": "Haunted Spooks (1920)",
+      "link": "http://fyeah-haroldlloyd.tumblr.com/post/121250053017/harold-lloyd-and-mildred-davis-haunted-spooks"
+    }],
+    // love action proposal
+    [{
+      "quote": "It’s too hot to play croquet— Let’s get married.",
+      "source": "A Sailor Made Man (1921)"
+    }, {
+      "quote": "To… romance!",
+      "source": "Our Modern Maidens (1929)"
+    }, {
+      "quote": "You are the first man who has accepted my sex challenge!",
+      "source": "The Farmer’s Wife (1928)"
+    }, {
+      "quote": "You know, something just told me… maybe we <i>will</i> get married.",
+      "source": "The Show (1927)"
+    }, {
+      "quote": "Oh! We’re forgetting to elope!",
+      "source": "The Matrimaniac (1916)"
+    }, {
+      "quote": "Kiss me, my Fool!",
+      "source": "A Fool There Was (1915)"
+    }, {
+      "quote": "Aren’t you going to undress me?",
+      "source": "Now or Never (1921)"
+    }],
+    // Kiss gif
+    [{
+      // Kiss 1
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845557/both_kiss_1_nhgrlq.gif",
+      "source": "Dr. Jack (1922)",
+      "link": "http://littlehorrorshop.tumblr.com/post/130658142458/an-emotional-farewell-between-mildred-davis-and"
+    }, {
+      // Kiss 2
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845556/both_kiss_2_h0gxd1.gif",
+      "source": "An Eastern Westerner (1920)",
+      "link": "http://fyeah-haroldlloyd.tumblr.com/post/53475356963/harold-lloyd-and-mildred-davis-an-eastern"
+    }, {
+      // Kiss 3
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845556/both_kiss_3_rxkw5s.gif",
+      "source": "Safety Last (1923)",
+      "link": "http://fyeah-haroldlloyd.tumblr.com/post/139011169097/fyeah-haroldlloyd-mildred-davis-and-harold"
+    }, {
+      // Kiss 4
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845557/both_kiss_4_hhjcgq.gif",
+      "source": "Safety Last (1923)",
+      "link": "http://mildreddavis.tumblr.com/post/27742884226"
+    }],
+    // The End gif/still
+    [{
+      // End 1
+      "media": "http://res.cloudinary.com/jessawright/image/upload/c_scale,w_500/v1458845560/end_1_nt2jou.jpg",
+      "source": "Speedy (1928)",
+      "link": "http://annyas.com/screenshots/updates/speedy-1928-harold-lloyd/"
+    }, {
+      // End 2
+      "media": "http://res.cloudinary.com/jessawright/image/upload/c_scale,w_500/v1458845560/end_2_jnvrdr.jpg",
+      "source": "The General (1926)",
+      "link": "http://annyas.com/screenshots/updates/general-1926-buster-keaton/"
+    }, {
+      // End 3
+      "media": "http://res.cloudinary.com/jessawright/image/upload/c_scale,w_500/v1458845560/end_3_rvolwn.jpg",
+      "source": "The Lodger (1927)",
+      "link": "http://annyas.com/screenshots/updates/lodger-1927-alfred-hitchcock/"
+    }, {
+      // End 4
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845559/end_4_cetwak.gif",
+      "source": "Grandma’s Boy (1922)",
+      "link": "http://fyeah-haroldlloyd.tumblr.com/post/131732717277/grandmas-boy-1922"
+    }, {
+      // End 5
+      "media": "http://res.cloudinary.com/jessawright/image/upload/v1458845560/end_5_vpnabm.gif",
+      "source": "I’m On My Way (1919)",
+      "link": "http://fyeah-haroldlloyd.tumblr.com/post/46884176244/im-on-my-way-1919"
+    }, {
+      // End 6
+      "media": "http://res.cloudinary.com/jessawright/image/upload/c_crop,g_center,h_375,w_500/v1458845560/end_6_vjzlxm.jpg",
+      "source": "The Thief of Bagdad (1924)",
+      "link": "http://annyas.com/screenshots/updates/thief-of-bagdad-1924-douglas-fairbanks/"
+    }]
+
+  ];
+  var a;
+  var sceneNum;
+
+  // Function to display text or media
+  function display() {
+    if (sceneNum == 1 || sceneNum == 3 || sceneNum == 5 || sceneNum == 7) {
+      $("#quotemedia").removeAttr("src");
+      $("#quotetext").html(quotes[sceneNum][a[sceneNum]].quote);
+      $("#sourcetext").html(quotes[sceneNum][a[sceneNum]].source);
+      $("#tweet").attr({
+        href: "https://twitter.com/intent/tweet?text=" + encodeURI("\"" + quotes[sceneNum][a[sceneNum]].quote + "\" from " + quotes[sceneNum][a[sceneNum]].source),
+        target: "_blank"
+      });
+    } else {
+      $("#quotemedia").attr({
+        src: quotes[sceneNum][a[sceneNum]].media
+      });
+      $("#quotetext").empty();
+      $("#sourcetext").html(quotes[sceneNum][a[sceneNum]].source + "<a href=\"" + quotes[sceneNum][a[sceneNum]].link + "\" target=\"_blank\">  (via)</a>");
+      $("#tweet").attr({
+        href: "https://twitter.com/intent/tweet?text=" + encodeURI("A scene from " + quotes[sceneNum][a[sceneNum]].source + ": " + quotes[sceneNum][a[sceneNum]].link),
+        target: "_blank"
+      });
+    };
+  };
+  // New Movie button
+  $("#newmovie").on("click", function() {
+    // Establish random number array
+    a = [];
+    sceneNum = 0;
+    for (var i = 0; i < quotes.length; i++) {
+      a.push(Math.floor(Math.random() * quotes[i].length));
+    };
+
+    display();
+    $("#nextscene").removeAttr("disabled");
+    $("#tweet").removeAttr("disabled");
+
+    console.log(a);
+  });
+
+  // Next Scene button
+  $("#nextscene").on("click", function() {
+    if (sceneNum < quotes.length - 2) {
+      sceneNum++;
+      display();
+    } else if (sceneNum == quotes.length - 2) {
+      sceneNum++;
+      display();
+      $("#nextscene").attr("disabled", "disabled");
+    };
+    if (sceneNum > 0) {
+      $("#lastscene").removeAttr("disabled");
+    };
+    console.log(sceneNum);
+  });
+
+  // Last Scene button
+  $("#lastscene").on("click", function() {
+    if (sceneNum > 0) {
+      sceneNum--;
+      display();
+    };
+    if (sceneNum == 0) {
+      $("#lastscene").attr("disabled", "disabled");
+    };
+    if (sceneNum < quotes.length - 1) {
+      $("#nextscene").removeAttr("disabled");
+    };
+    console.log(sceneNum);
+  })
+
+  // Tweet button
+
+});
